770ef23ea118eeeb0bd3596418b91fec72b3354b
Anarbek qa <anarbek.ind@gmail.com>
2026-08-17 02:08:33 +0500

    sprint_2

---
+++ task_5.py
@@ -0,0 +1,53 @@
+class Results:
+
+    def __init__(self, victories, draws, losses):
+        self.victories = victories
+        self.draws = draws
+        self.losses = losses
+
+
+class Football(Results):
+
+    def number_of_wins(self):
+        return f"Футбольных побед: {self.victories}"
+
+    def number_of_draws(self):
+        return f"Футбольных ничьих: {self.draws}"
+
+    def number_of_losses(self):
+        return f"Футбольных поражений: {self.losses}"
+
+    def total_points(self):
+        return f"Общее количество очков: {3 * self.victories + self.draws}"
+
+
+class Hockey(Results):
+
+    def number_of_wins(self):
+        return f"Хоккейных побед: {self.victories}"
+
+    def number_of_draws(self):
+        return f"Хоккейных ничьих: {self.draws}"
+
+    def number_of_losses(self):
+        return f"Хоккейных поражений: {self.losses}"
+
+    def total_points(self):
+        return f"Общее количество очков: {2 * self.victories + self.draws}"
+
+
+football_team = Football(2, 2, 2)
+hockey_team = Hockey(2, 2, 2)
+
+
+for method in [
+    football_team.number_of_wins,
+    football_team.number_of_draws,
+    football_team.number_of_losses,
+    football_team.total_points,
+    hockey_team.number_of_wins,
+    hockey_team.number_of_draws,
+    hockey_team.number_of_losses,
+    hockey_team.total_points
+]:
+    print(method())
